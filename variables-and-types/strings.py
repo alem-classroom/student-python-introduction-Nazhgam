1d728dc4db066b30fc043b47161095b91430ecef
Nazhgam <52975418+Nazhgam@users.noreply.github.com>
2020-08-02 11:00:42 +0600

    strings
    nothing to say

--- variables-and-types/strings.py
+++ variables-and-types/strings.py
@@ -1,15 +1,18 @@
 def size_of_str(str):
-    # return size of string
+    return len(str)
 
 def concat_strings(first, second):
-    # return concatination of first and second strings
+    return first + second
 
 def duplicate_string(str, copy):
+    return repeat(object[str, copy])
     # return new string which is copy of str copy times
     # example -> duplicate_string('s', 2) == 'ss'
 
 def reverse(str):
+    return reversed(str)
     # return reverse of the string
 
 def is_substring(str, substr):
+    return substr in str
     # return true if substr is the substring of str, false otherwise
