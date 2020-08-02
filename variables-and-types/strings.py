0af96d68e35f8c52fe3dfc6f499d398449d5eb3a
Nazhgam <52975418+Nazhgam@users.noreply.github.com>
2020-08-02 16:08:12 +0600

    Update strings.py

--- variables-and-types/strings.py
+++ variables-and-types/strings.py
@@ -5,23 +5,23 @@ def size_of_str(str):
 
 def concat_strings(first, second):
       ans = first + second
-return ans
+      return ans
     # return concatination of first and second strings
 
 def duplicate_string(str, copy):
-ans = str*copy      
-return ans
+        ans = str*copy
+        return str*copy
     # return new string which is copy of str copy times
     # example -> duplicate_string('s', 2) == 'ss'
 
 def reverse(str):
-ans = str[::-1]    
-return ans
+        ans = str[::-1]
+        return ans
     # return reverse of the string
 
 def is_substring(str, substr):
-ans = substr in str     
-return ans
+        ans = substr in str
+        return ans
     # return true if substr is the substring of str, false otherwise
 
 
