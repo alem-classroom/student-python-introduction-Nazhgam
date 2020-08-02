cc25025dcde307f262536e8f0d4b350045362eaa
Nazhgam <52975418+Nazhgam@users.noreply.github.com>
2020-08-02 16:16:59 +0600

    p1

--- variables-and-types/calculator.py
+++ variables-and-types/calculator.py
@@ -1,20 +1,20 @@
 def add(first, second):
-    ans=first+second
+    ans = first + second
     return ans
 
 def sub(first, second):
-       ans=first-second
+    ans = first - second
     return ans
 
 def multiply(first, second):
-        ans=first*second
+    ans = first * second
     return ans
 
 def div_with_floor(first, second):
-        ans=first//second
+    ans = first // second
     return ans
 
 
 def div_without_floor(first, second):
-      ans=first/second
+    ans = first / second
     return ans
